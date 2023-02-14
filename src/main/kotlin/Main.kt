28a9ae6181a60829417bb5c4e44edc704da2bc1c
enderdissa <secondissa@yandex.ru>
2023-02-15 02:57:05 +0300

    test console-handle branch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ import movies.*
  * @param args an array of console arguments
  */
 fun main(args: Array<String>) {
-    println("world!")
+    println("worldss!")
     var m = Person(name = "string", height = 10f, hairColor = Color.WHITE, nationality = Country.RUSSIA)
 
     var k = Movie(name = "Str", coordinates = Coordinates(5.1, 5f), oscarsCount = 1, length = 1, genre = MovieGenre.COMEDY, mpaaRating = MpaaRating.PG_13)
